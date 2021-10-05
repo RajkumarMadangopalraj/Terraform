c815413cba2cdcf39bd515ee7034737ca0ee37af
Rajkumar Madangopalraj <rajkumar@rajkumar.local>
2021-10-05 21:22:10 +0530

    Adding Compute_Instances and VPC_Network

--- VPC_Network/credentials.tf
+++ VPC_Network/credentials.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  credentials = file("/Users/rajkumar/Desktop/Mystuffs/Terraform/Key/kubernetes-practise-326615-a00f95561a30.json")
+  credentials = file("/PATH/TO/YOUR/FILE.json")
 
-  project = "kubernetes-practise-326615"
+  project = "PROJECTNAME"
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = "us-central1-a"
 }
